4fc9229218e325da1968c2c497c2132b79f1053a
gseijas1305 <87491052+gseijas1305@users.noreply.github.com>
2022-04-19 20:49:01 -0600

    Creacion hasta Movements

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,28 @@
 <template>
-  <div>Hola Mundo</div>
+  <Layout>
+    <template #header>
+      <Header></Header>
+    </template>
+    <template #resume>
+      <Resume></Resume>
+    </template>
+    <template #movements>
+      <Movements />
+    </template>
+  </Layout>
 </template>
+
+<script>
+import Layout from "./Layout.vue";
+import Header from "./Header.vue";
+import Resume from "./Resume/Index.vue";
+import Movements from "./Movements.vue";
+export default {
+  components: {
+    Layout,
+    Header,
+    Resume,
+    Movements,
+  },
+};
+</script>
---
+++ src/components/Resume/Index.vue
@@ -0,0 +1,3 @@
+<template>
+  <main>Resumen</main>
+</template>
